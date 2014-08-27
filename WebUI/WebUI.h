8e7271a8b231a4d6c772d1dc0abf2d3d6e2bef47
Neil Stephens or Alan Murray <VisualStudioWorkstation@fakeemail.com>
2014-08-27 17:47:59 +1000

    Added beginning of a libmicrohttpd web user interface

---
+++ WebUI/WebUI.h
@@ -0,0 +1,49 @@
+/*	opendatacon
+ *
+ *	Copyright (c) 2014:
+ *
+ *		DCrip3fJguWgVCLrZFfA7sIGgvx1Ou3fHfCxnrz4svAi
+ *		yxeOtDhDCXf1Z4ApgXvX5ahqQmzRfJ2DoX8S05SqHA==
+ *	
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */ 
+class WebUI
+{
+public:
+	WebUI();
+
+	/* HTTP static response handler call back */
+	static int ahc_echo(void * cls,
+	struct MHD_Connection * connection,
+		const char * url,
+		const char * method,
+		const char * version,
+		const char * upload_data,
+		size_t * upload_data_size,
+		void ** ptr);
+
+	/* HTTP file handler call back */
+	static int
+		http_ahc(void *cls,
+	struct MHD_Connection *connection,
+		const char *url,
+		const char *method,
+		const char *version,
+		const char *upload_data,
+		size_t *upload_data_size, void **ptr);
+
+	int start(uint16_t port);
+	void stop();
+private:
+	struct MHD_Daemon * d;
+};
