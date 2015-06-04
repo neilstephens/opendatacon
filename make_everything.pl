1c96f286aa09b26302bdefb4bc958f42d4a3d09a
Neil Stephens <dearknarl@gmail.com>
2015-06-04 12:35:43 +1000

    added pretty printing into the make_everything script

--- make_everything.pl
+++ make_everything.pl
@@ -6,18 +6,29 @@ use File::Find::Rule;
 my @subdirs = File::Find::Rule->directory->in("./");
 
 open MAKELOG, ">make.log";
+my $failed = 0;
 
 my %subdirs_hash;
 foreach my $dir (@subdirs)
 {
 	if($dir =~ /(Debug|Release)(-rpi|-RHEL65)?$/)
 	{
-		my $out = `(cd $dir && echo "$dir" && date && make 2>&1)`;
-		print MAKELOG $out;
+		my $out = `(cd $dir && echo "\\n\\n--------------------\\n$dir" && date && echo "--------------------\\n" && make 2>&1)`;
 		if(${^CHILD_ERROR_NATIVE})
 		{
-			print $out;
+			$failed = 1;
 		}
+		print $out;
+		print MAKELOG $out;
 	}
+}
+
+if($failed == 1)
+{
+	print "\n\n\n FAIL !!!!!!!!!!!!!! \n\n\n";
+}
+else
+{
+	print "\n\n\n DONE \n\n\n";
 }
 
