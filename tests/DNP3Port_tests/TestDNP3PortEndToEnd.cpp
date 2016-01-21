5289a4faaf68dd2afb933f5ddb193d1bb2b5436e
Neil Stephens <dearknarl@gmail.com>
2016-01-21 17:27:56 +1100

    Filled out the DNP3Port end-to-end test - not fully working yet (link status doesn't change, but master scans outstation)

--- tests/DNP3Port_tests/TestDNP3PortEndToEnd.cpp
+++ tests/DNP3Port_tests/TestDNP3PortEndToEnd.cpp
@@ -29,19 +29,46 @@
 
 TEST_CASE(SUITE("ConstructEnableDisableDestroy"))
 {
-	fptr newMaster = GetPortCreator("DNP3Port", "DNP3Master");
-	REQUIRE(newMaster);
-	DataPort* MPUT = newMaster("MasterUnderTest", "", "");
+	//make an outstation port
 	fptr newOutstation = GetPortCreator("DNP3Port", "DNP3Outstation");
 	REQUIRE(newOutstation);
-	DataPort* OPUT = newOutstation("OutstationUnderTest", "", "");
 
-	MPUT->Enable();
+	Json::Value Oconf;
+	Oconf["IP"] = "0.0.0.0";
+	DataPort* OPUT = newOutstation("OutstationUnderTest", "", Oconf);
+
+	//make a master port
+	fptr newMaster = GetPortCreator("DNP3Port", "DNP3Master");
+	REQUIRE(newMaster);
+
+	Json::Value Mconf;
+	Mconf["ServerType"] = "PERSISTENT";
+	DataPort* MPUT = newMaster("MasterUnderTest", "", Mconf);
+
+	auto pDNP3Man = new asiodnp3::DNP3Manager(std::thread::hardware_concurrency());
+	auto pLOG_LEVEL = new openpal::LogFilters();
+
+	//get them to build themselves using their configs
+	OPUT->BuildOrRebuild(*pDNP3Man,*pLOG_LEVEL);
+	MPUT->BuildOrRebuild(*pDNP3Man,*pLOG_LEVEL);
+
+	//turn them on
 	OPUT->Enable();
+	MPUT->Enable();
 
-	auto mstatus = MPUT->GetStatus();
-	auto ostatus = OPUT->GetStatus();
+	//TODO: write a better way to wait for GetStatus and timeout (when decouple gets merged)
+	uint count = 0;
+	while(OPUT->GetStatus()["Result"].asString() == "Port enabled - link down" && count < 10000)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		count++;
+	}
+
+	REQUIRE(MPUT->GetStatus()["Result"].asString() == "Port enabled - link up (unreset)");
+	REQUIRE(OPUT->GetStatus()["Result"].asString() == "Port enabled - link up (unreset)");
 
 	delete MPUT;
 	delete OPUT;
+	delete pLOG_LEVEL;
+	delete pDNP3Man;
 }
