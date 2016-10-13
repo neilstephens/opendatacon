e1a42ab7bebe696aa8375e2e0ffaa8fdc49002de
Neil Stephens <dearknarl@gmail.com>
2016-10-14 10:51:07 +1100

    Added a DNP3 serial port end-to-end unit test

--- tests/DNP3Port_tests/TestDNP3PortEndToEnd.cpp
+++ tests/DNP3Port_tests/TestDNP3PortEndToEnd.cpp
@@ -27,7 +27,7 @@
 
 #define SUITE(name) "DNP3PortEndToEndTestSuite - " name
 
-TEST_CASE(SUITE("ConstructEnableDisableDestroy"))
+TEST_CASE(SUITE("TCP link"))
 {
 	//make an outstation port
 	fptr newOutstation = GetPortCreator("DNP3Port", "DNP3Outstation");
@@ -43,6 +43,79 @@ TEST_CASE(SUITE("ConstructEnableDisableDestroy"))
 
 	Json::Value Mconf;
 	Mconf["ServerType"] = "PERSISTENT";
+	DataPort* MPUT = newMaster("MasterUnderTest", "", Mconf);
+
+	asiodnp3::DNP3Manager lDNP3Man(std::thread::hardware_concurrency());
+	openpal::LogFilters lLOG_LEVEL;
+
+	//get them to build themselves using their configs
+	OPUT->BuildOrRebuild(lDNP3Man,lLOG_LEVEL);
+	MPUT->BuildOrRebuild(lDNP3Man,lLOG_LEVEL);
+
+	//turn them on
+	OPUT->Enable();
+	MPUT->Enable();
+
+	//TODO: write a better way to wait for GetStatus and timeout (when decouple gets merged)
+	unsigned int count = 0;
+	while((MPUT->GetStatus()["Result"].asString() == "Port enabled - link down" || OPUT->GetStatus()["Result"].asString() == "Port enabled - link down") && count < 5000)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		count++;
+	}
+
+	REQUIRE(MPUT->GetStatus()["Result"].asString() == "Port enabled - link up (unreset)");
+	REQUIRE(OPUT->GetStatus()["Result"].asString() == "Port enabled - link up (unreset)");
+
+	//turn outstation off
+	OPUT->Disable();
+
+	count = 0;
+	while(MPUT->GetStatus()["Result"].asString() == "Port enabled - link up (unreset)" && count < 5000)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		count++;
+	}
+
+	REQUIRE(MPUT->GetStatus()["Result"].asString() == "Port enabled - link down");
+	REQUIRE(OPUT->GetStatus()["Result"].asString() == "Port disabled");
+
+	delete MPUT;
+	delete OPUT;
+}
+
+TEST_CASE(SUITE("Serial link"))
+{
+	int i;
+	if (!system(NULL))
+	{
+		WARN("Can't get system shell to execute socat (for virtual serial ports) - skipping test");
+		return;
+	}
+	if(system("socat pty,raw,echo=0,link=SerialEndpoint1 pty,raw,echo=0,link=SerialEndpoint2 &"))
+	{
+		WARN("Failed to execute socat (for virtual serial ports) - skipping test");
+		return;
+	}
+
+
+	//make an outstation port
+	fptr newOutstation = GetPortCreator("DNP3Port", "DNP3Outstation");
+	REQUIRE(newOutstation);
+
+	Json::Value Oconf;
+	Oconf["SerialDevice"] = "SerialEndpoint1";
+	DataPort* OPUT = newOutstation("OutstationUnderTest", "", Oconf);
+
+	//make a master port
+	fptr newMaster = GetPortCreator("DNP3Port", "DNP3Master");
+	REQUIRE(newMaster);
+
+	Json::Value Mconf;
+	Mconf["ServerType"] = "PERSISTENT";
+	Mconf["SerialDevice"] = "SerialEndpoint2";
+	Mconf["LinkKeepAlivems"] = 200;
+	Mconf["LinkTimeoutms"] = 100;
 	DataPort* MPUT = newMaster("MasterUnderTest", "", Mconf);
 
 	asiodnp3::DNP3Manager lDNP3Man(std::thread::hardware_concurrency());
