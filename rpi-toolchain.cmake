b40ac2ddc69e637355468dedb6b6ba3b0c48bda3
Neil Stephens <dearknarl@gmail.com>
2018-11-25 12:01:55 +1100

    update rpi toolchain file to use stock ubuntu cross compiler 	- and also look for libs in sysroot properly

--- rpi-toolchain.cmake
+++ rpi-toolchain.cmake
@@ -5,21 +5,17 @@ SET(CMAKE_SYSTEM_PROCESSOR armv7)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER
-"/home/knarl/x-tools/armv7-rpi-linux-gnueabihf-2.21_5.1/bin/armv7-rpi-linux-gnueabihf-gcc")
+SET(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
 
-SET(CMAKE_CXX_COMPILER
-"/home/knarl/x-tools/armv7-rpi-linux-gnueabihf-2.21_5.1/bin/armv7-rpi-linux-gnueabihf-g++")
+SET(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH
-"/home/knarl/x-tools/armv7-rpi-linux-gnueabihf-2.21_5.1/armv7-rpi-linux-gnueabihf/sysroot/")
-
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} "/home/knarl/qtc_projects/sysroot/")
+SET(CMAKE_FIND_ROOT_PATH "$ENV{RPI_SYSROOT}")
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} "/usr/arm-linux-gnueabihf/")
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY) 
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH) 
 
