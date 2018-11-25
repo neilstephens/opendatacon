4ef84138352e24eb6ee6cb78e3ba7bc168456f01
Neil Stephens <dearknarl@gmail.com>
2018-11-25 14:34:02 +1100

    force rpi cross compiler lib arch [skip appveyor]

--- rpi-toolchain.cmake
+++ rpi-toolchain.cmake
@@ -6,7 +6,6 @@ SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
 SET(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
-
 SET(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 
 # where is the target environment
@@ -18,4 +17,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH) 
+
+#this isn't detected for some compiler versions
+set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
 
