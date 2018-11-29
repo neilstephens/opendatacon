2ef4a05442b30c3e12bb5514457427401d406ccc
Neil Stephens <dearknarl@gmail.com>
2018-11-29 21:22:34 +1100

    cache cxx flags in 32bit fake toolchain file

--- i386-toolchain.cmake
+++ i386-toolchain.cmake
@@ -5,7 +5,7 @@ SET(CMAKE_SYSTEM_PROCESSOR i386)
 SET(CMAKE_SYSTEM_VERSION generic)
 
 # not actually cross compiling, just -m32
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+SET(CMAKE_CXX_FLAGS "-m32" CACHE STRING "...")
 
 #this isn't detected for some compiler versions
 set(CMAKE_LIBRARY_ARCHITECTURE i386-linux-gnu)
