a8663048ac075ea6785a457cf2f834a45e3e6eb3
Neil Stephens <dearknarl@gmail.com>
2018-11-29 22:26:06 +1100

    32bit build tweaks

--- i386-toolchain.cmake
+++ i386-toolchain.cmake
@@ -6,6 +6,7 @@ SET(CMAKE_SYSTEM_VERSION generic)
 
 # not actually cross compiling, just -m32
 SET(CMAKE_CXX_FLAGS "-m32" CACHE STRING "...")
+SET(CMAKE_C_FLAGS "-m32" CACHE STRING "...")
 
 #this isn't detected for some compiler versions
 set(CMAKE_LIBRARY_ARCHITECTURE i386-linux-gnu)
