5893f4f0766416b0c758286de40fbeba55aba6b3
Neil Stephens <dearknarl@gmail.com>
2018-11-29 14:53:29 +1100

    i686 and enterprise linux v6 builds

--- i686-toolchain.cmake
+++ i686-toolchain.cmake
@@ -1,11 +1,11 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_PROCESSOR armhf)
-SET(CMAKE_SYSTEM_VERSION rpi)
+SET(CMAKE_SYSTEM_PROCESSOR i686)
+SET(CMAKE_SYSTEM_VERSION generic)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
-SET(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
+SET(CMAKE_C_COMPILER "i686-linux-gnu-gcc")
+SET(CMAKE_CXX_COMPILER "i686-linux-gnu-g++")
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH "$ENV{SYSROOT}")
@@ -15,8 +15,8 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
-SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH) 
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
 #this isn't detected for some compiler versions
-set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+set(CMAKE_LIBRARY_ARCHITECTURE i686-linux-gnu)
 
