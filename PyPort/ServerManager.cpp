24e20a6d6b2ca5240d1827cb9d023880e43a287a
Neil Stephens <dearknarl@gmail.com>
2019-07-17 11:15:20 +1000

    change ServerManager::ServerMap to store weak_ptrs for automatic, race-free cleanup

--- PyPort/ServerManager.cpp
+++ PyPort/ServerManager.cpp
@@ -33,28 +33,12 @@
 #include "ServerManager.h"
 
 
-std::unordered_map<std::string, std::shared_ptr<ServerManager>> ServerManager::ServerMap;
+std::unordered_map<std::string, std::weak_ptr<ServerManager>> ServerManager::ServerMap;
 std::mutex ServerManager::MapMutex; // Control access
 
 
 ServerTokenType::~ServerTokenType()
-{
-	// If we are the last ServerToken for this connection (usecount == 2 , the Map and us) - delete the connection.
-	if (pServerManager)
-	{
-		if (pServerManager.use_count() <= 2)
-		{
-			LOGDEBUG("Use Count On ServerTok Shared_ptr down to 2 - Destroying the Map Server - {}", ServerID);
-			pServerManager->RemoveServerFromMap();
-			// Now release our shared_ptr - the last one..The ServerManager destructor should now be called.
-			pServerManager.reset();
-		}
-		else
-		{
-			LOGDEBUG("Use Count On Connection Shared_ptr at {} - The Map Server Stays - {}", pServerManager.use_count(), ServerID);
-		}
-	}
-}
+{}
 
 ServerManager::ServerManager(std::shared_ptr<odc::asio_service> apIOS, const std::string& aEndPoint, const std::string& aPort):
 	pIOS(apIOS),
@@ -62,7 +46,7 @@ ServerManager::ServerManager(std::shared_ptr<odc::asio_service> apIOS, const std
 	Port(aPort)
 {
 
-	pServer.reset(new http::server (pIOS, EndPoint, Port));
+	pServer = std::make_shared<http::server>(pIOS, EndPoint, Port);
 
 	InternalServerID = MakeServerID(aEndPoint, aPort);
 
@@ -76,18 +60,20 @@ ServerTokenType ServerManager::AddConnection(std::shared_ptr<odc::asio_service> 
 
 	std::unique_lock<std::mutex> lck(ServerManager::MapMutex); // Access the Map - only here and in the destructor do we do this.
 
-	// Only add if does not exist
-	if (ServerMap.count(ServerID) == 0)
+	// Only add if does not exist, or has expired
+	std::shared_ptr<ServerManager> pSM;
+	if (ServerMap.count(ServerID) == 0 || !(pSM = ServerMap[ServerID].lock()))
 	{
-		// If we give each ServerToken a shared_ptr to the connection, then in the ServerToken destructors,
-		// when the use_count is 2 (the map and the ServerToken), then it is time to destroy the connection.
-
-		ServerMap[ServerID] = std::make_shared<ServerManager>(apIOS, aEndPoint, aPort);
+		LOGDEBUG("First ServerTok for connection - {}", ServerID);
+		// If we give each ServerToken a shared_ptr to the connection, then the
+		//connection gets destoyed with the last token
+		pSM = std::make_shared<ServerManager>(apIOS, aEndPoint, aPort);
+		ServerMap[ServerID] = pSM;
 	}
 	else
 		LOGDEBUG("ServerTok already exists, using that connection - {}", ServerID);
 
-	return ServerTokenType(ServerID, ServerMap[ServerID]); // the use count on the Connection Map shared_ptr will go up by one.
+	return ServerTokenType(ServerID, pSM);
 }
 
 void ServerManager::StartConnection(const ServerTokenType& ServerTok)
@@ -125,15 +111,6 @@ void ServerManager::AddHandler(const ServerTokenType& ServerTok, const std::stri
 		LOGERROR("Tried to add a urihandler when the httpserver was no longer valid");
 	}
 }
-
-
-// Use this method only to shut down a connection and destroy it. Only used internally
-void ServerManager::RemoveServerFromMap()
-{
-	std::unique_lock<std::mutex> lck(ServerManager::MapMutex);
-	ServerMap.erase(InternalServerID); // Remove the map entry shared ptr - so that shared ptr no longer points to us.
-}
-
 
 ServerManager::~ServerManager()
 {
