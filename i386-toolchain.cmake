b2ea846d72b6309f2ccc9fa44a47d8d66782763c
Neil Stephens <dearknarl@gmail.com>
2019-07-20 23:26:40 +1000

    python needs x86 not i386 as processor

--- i386-toolchain.cmake
+++ i386-toolchain.cmake
@@ -1,7 +1,7 @@
 #use a real cross compiler when travis support > ubuntu bionic 
 
 SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_PROCESSOR i386)
+SET(CMAKE_SYSTEM_PROCESSOR x86)
 SET(CMAKE_SYSTEM_VERSION generic)
 
 # not actually cross compiling, just -m32
