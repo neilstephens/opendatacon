f2fea61ae03d9e7e296d228c45f7e652619101aa
Neil Stephens <dearknarl@gmail.com>
2019-07-22 15:03:36 +1000

    a few more tweaks to cross compiling

--- i386-toolchain.cmake
+++ i386-toolchain.cmake
@@ -7,6 +7,7 @@ SET(CMAKE_SYSTEM_VERSION generic)
 # not actually cross compiling, just -m32
 SET(CMAKE_CXX_FLAGS "-m32" CACHE STRING "...")
 SET(CMAKE_C_FLAGS "-m32" CACHE STRING "...")
+SET(CMAKE_ASM_FLAGS "-m32" CACHE STRING "...")
 
 #this isn't detected for some compiler versions
 set(CMAKE_LIBRARY_ARCHITECTURE i386-linux-gnu)
