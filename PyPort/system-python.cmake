8aaf025643260c8e7a2a83a82e6eaaa9a2d6cc7f
Neil Stephens <dearknarl@gmail.com>
2019-07-24 09:37:44 +1000

    Reset PYTHON_HOME if it's set too 'high' 	this accounts for /usr/local instead of /usr 	and osx framework under /usr

--- PyPort/system-python.cmake
+++ PyPort/system-python.cmake
@@ -10,23 +10,36 @@ if(NOT USE_PYTHON_SUBMODULE)
 	file(GLOB_RECURSE PYTHON_H ${CMAKE_FIND_ROOT_PATH}/${PYTHON_HOME}/*Python.h)
 	if(PYTHON_H)
 		message("Found Python.h using CMAKE_FIND_ROOT_PATH")
+		set(TOP_INCLUDE ${CMAKE_FIND_ROOT_PATH}/${PYTHON_HOME}/include)
 	else()
+		set(TOP_INCLUDE ${PYTHON_HOME}/include)
 		file(GLOB_RECURSE PYTHON_H ${PYTHON_HOME}/*Python.h)
 	endif()
 	message("Globbed ${PYTHON_H}")
-	string(REGEX MATCH "python3.([0-9])+m" PYTHON_VER "${PYTHON_H}")
-	set(PYTHON_MINOR_VER ${CMAKE_MATCH_1})
+	string(REGEX MATCH "([^;]*)/include/(python3.([0-9])+m)" PYTHON_VER "${PYTHON_H}")
+	set(PYTHON_HOME_DISCOVERED ${CMAKE_MATCH_1})
+	set(PYTHON_VER ${CMAKE_MATCH_2})
+	set(PYTHON_MINOR_VER ${CMAKE_MATCH_3})
 	message("Version string: ${PYTHON_VER}")
+	if(${PYTHON_HOME_DISCOVERED} EQUAL ${PYTHON_HOME})
+		message("PYTHON_HOME confirmed")
+	else()
+		message("Warning: resetting PYTHON_HOME to ${PYTHON_HOME_DISCOVERED}")
+		set(PYTHON_HOME ${PYTHON_HOME_DISCOVERED})
+	endif()
+
 	find_path(PYTHON_INCLUDE_DIRS Python.h
-		PATHS ${PYTHON_HOME}/include/${PYTHON_VER} ${PYTHON_HOME}/local/include/${PYTHON_VER}
+		PATHS ${PYTHON_HOME}/include/${PYTHON_VER}
 		NO_DEFAULT_PATH
 		CMAKE_FIND_ROOT_PATH_BOTH)
 
 	if(PYTHON_INCLUDE_DIRS)
 		message("Python headers found: ${PYTHON_INCLUDE_DIRS}")
 	else()
-		message("Can't find Python headers in ${PYTHON_HOME}" )
+		message(FATAL_ERROR "Can't find Python headers in ${PYTHON_HOME}" )
 	endif()
+
+	set(PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS} ${TOP_INCLUDE})
 
 	string(REGEX MATCH "3[0-9]+" PYTHON_NUM "${PYTHON_INCLUDE_DIRS}")
 
