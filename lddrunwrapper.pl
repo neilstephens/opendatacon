770faf5dada989983ac6d105b29451f211534590
Neil Stephens <dearknarl@gmail.com>
2019-07-31 15:40:09 +1000

    fix ldd run wrapper to only change docker local paths

--- lddrunwrapper.pl
+++ lddrunwrapper.pl
@@ -5,6 +5,7 @@
 
 my $run_cmd = $ENV{'RUN'} or "";
 my $sysroot = $ENV{'SYSROOT'} or "";
+my $src_dir = $ENV{'SRC_DIR'} or "";
 my $ldd_args = join(' ',@ARGV);
 
 my $ldd_out = `$run_cmd ldd $ldd_args`;
@@ -12,11 +13,20 @@ my $ldd_out = `$run_cmd ldd $ldd_args`;
 my $ldd_return = $? >> 8;
 
 $sysroot =~ s/\/$//; #remove trailing slash
-#prepend sysroot, unless it's already there
-$ldd_out =~ s/(\s=>\s)(\Q$sysroot\E)?(\/)/$1$sysroot$3/gs;
+$src_dir =~ s/\/$//; #remove trailing slash
+
+#prepend sysroot if it's a docker local path
+my @ldd_lines = split(/^/,$ldd_out);
+for my $i (0 .. (scalar @ldd_lines -1))
+{
+	if(not $ldd_lines[$i] =~ /\s=>\s\Q$src_dir\E/)
+	{
+		$ldd_lines[$i] =~ s/(\s=>\s)(\/)/$1$sysroot$2/;
+	} 
+}
 
 #print the fixed output
-print $ldd_out;
+print @ldd_lines;
 
 #return the same value
 exit $ldd_return;
