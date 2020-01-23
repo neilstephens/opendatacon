2d79f588cb00c876a3dff0a539598455952c33d6
Neil Stephens <dearknarl@gmail.com>
2020-01-24 08:32:04 +1100

    TCP keepalives for osx

--- include/opendatacon/Platform.h
+++ include/opendatacon/Platform.h
@@ -216,8 +216,17 @@ inline void SetTCPKeepalives(asio::ip::tcp::socket& tcpsocket, bool enable=true,
 	alive.keepalivetime = initial_interval_s * 1000;
 	alive.keepaliveinterval = subsequent_interval_s * 1000;
 
-	if(WSAIoctl(tcpsocket.native_handle(), SIO_KEEPALIVE_VALS, &alive, sizeof(alive), NULL, 0, &dwBytesRet, nullptr, nullptr) == SOCKET_ERROR)
+	if(WSAIoctl(tcpsocket.native_handle(), SIO_KEEPALIVE_VALS, &alive, sizeof(alive), nullptr, 0, &dwBytesRet, nullptr, nullptr) == SOCKET_ERROR)
 		throw std::runtime_error("Failed to set TCP SIO_KEEPALIVE_VALS");
+}
+#elif __APPLE__
+inline void SetTCPKeepalives(asio::ip::tcp::socket& tcpsocket, bool enable=true, unsigned int initial_interval_s=7200, unsigned int subsequent_interval_s=75, unsigned int fail_count=9)
+{
+	int set = enable ? 1 : 0;
+	if(setsockopt(sockfd, SOL_SOCKET,  SO_KEEPALIVE, &set, sizeof(set)) != 0)
+		throw std::runtime_error("Failed to set TCP SO_KEEPALIVE");
+	if(setsockopt(sockfd, IPPROTO_TCP, TCP_KEEPALIVE, &initial_interval_s, sizeof(initial_interval_s)) != 0)
+		throw std::runtime_error("Failed to set TCP_KEEPALIVE");
 }
 #else
 inline void SetTCPKeepalives(asio::ip::tcp::socket& tcpsocket, bool enable=true, unsigned int initial_interval_s=7200, unsigned int subsequent_interval_s=75, unsigned int fail_count=9)
