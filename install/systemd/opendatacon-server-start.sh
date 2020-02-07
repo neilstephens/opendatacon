ce1070569a4c0b61d3815210c4b6b376731d6b96
SCOTT10LT\scott <scott.ellis@novatex.com.au>
2020-02-07 12:25:47 +1100

    One mistake...

--- install/systemd/opendatacon-server-start.sh
+++ install/systemd/opendatacon-server-start.sh
@@ -4,6 +4,6 @@
 # We copy any python files in the config file/working folder to the bin folder where opendatacon will look for them if required
 #
 source scl_source enable rh-python36
-/bin/cp -rf /etc/opendatacon/*.py /opt/ODC/bin/*.py
+/bin/cp -rf /etc/opendatacon/*.py /opt/ODC/bin/
 LD_LIBRARY_PATH=/usr/local/lib 
 exec /opt/ODC/bin/opendatacon -p /etc/opendatacon
