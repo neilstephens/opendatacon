e505fa733eb8a776e6fa2ca5b1f03bc5fc5bb9d1
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-06-19 23:48:32 +1000

    UpdateInterval, SetPoint and ForcePoint working

--- WebUI/WebUI.cpp
+++ WebUI/WebUI.cpp
@@ -298,30 +298,32 @@ void WebUI::Disable()
 std::string WebUI::HandleSimControl(const std::string& url)
 {
 	const std::string simcontrol = "/SimControl";
-	const std::string command = url.substr(simcontrol.size() + 1, url.size() - simcontrol.size());
+	std::string command = url.substr(simcontrol.size() + 1, url.size() - simcontrol.size());
 
 	Json::Value value;
 	std::string data;
+	ParamCollection params;
 
-	if (command == "List")
+	if (command != "List")
 	{
-		ParamCollection params;
-		value = Responders[simcontrol]->ExecuteCommand(command, params);
-	}
-	else
-	{
-		ParamCollection params;
+		std::string ports, type, index, value;
 		std::istringstream iss(command);
-		std::string sub_command, temp;
-		iss >> sub_command;
-		iss >> temp;
-		iss >> params["0"];
-		iss >> params["1"];
-		iss >> params["2"];
-		iss >> params["Target"];
-		value = Responders[simcontrol]->ExecuteCommand(sub_command, params);
+		iss >> command;
+		iss >> ports;
+		iss >> type;
+		iss >> index;
+		params["Target"] = ports;
+		params["0"] = type;
+		params["1"] = index;
+
+		if (command != "ReleasePoint")
+		{
+			iss >> value;
+			params["2"] = value;
+		}
 	}
 
+	value = Responders[simcontrol]->ExecuteCommand(command, params);
 	Json::StreamWriterBuilder wbuilder;
 	std::unique_ptr<Json::StreamWriter> const pWriter(wbuilder.newStreamWriter());
 	std::ostringstream oss;
