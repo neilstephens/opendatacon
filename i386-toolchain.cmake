c25656aa94a599f9821b7dcec973ed4607e30316
Neil Stephens <dearknarl@gmail.com>
2020-06-25 21:51:38 +1000

    switch to cross-compiling for i686 travis [skip appveyor]

--- i386-toolchain.cmake
+++ i386-toolchain.cmake
@@ -1,14 +1,22 @@
-#use a real cross compiler when travis support > ubuntu bionic 
-
+# this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR x86)
 SET(CMAKE_SYSTEM_VERSION generic)
 
-# not actually cross compiling, just -m32
-SET(CMAKE_CXX_FLAGS "-m32" CACHE STRING "...")
-SET(CMAKE_C_FLAGS "-m32" CACHE STRING "...")
-SET(CMAKE_ASM_FLAGS "-m32" CACHE STRING "...")
+# specify the cross compiler
+SET(CMAKE_ASM_COMPILER "i686-linux-gnu-gcc-7")
+SET(CMAKE_C_COMPILER "i686-linux-gnu-gcc-7")
+SET(CMAKE_CXX_COMPILER "i686-linux-gnu-g++-7")
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH "$ENV{SYSROOT}")
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY) 
 
 #this isn't detected for some compiler versions
-set(CMAKE_LIBRARY_ARCHITECTURE i386-linux-gnu)
+set(CMAKE_LIBRARY_ARCHITECTURE i686-linux-gnu)
 
