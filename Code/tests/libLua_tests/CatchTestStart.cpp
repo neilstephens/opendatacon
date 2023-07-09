ca055339bdc1b013f7fb9ad9640485d9dba9a601
Neil Stephens <dearknarl@gmail.com>
2023-07-09 23:44:51 +1000

    start libLua tests

--- Code/tests/libLua_tests/CatchTestStart.cpp
+++ Code/tests/libLua_tests/CatchTestStart.cpp
@@ -22,7 +22,6 @@
 
 #define CATCH_CONFIG_RUNNER
 
-#include "../../../opendatacon/DataConnector.cpp"
 #include <opendatacon/util.h>
 #include <catch.hpp>
 
@@ -38,7 +37,7 @@ int main( int argc, char* argv[] )
 		log_level = spdlog::level::from_str(level_str);
 		if (log_level == spdlog::level::off && level_str != "off")
 		{
-			std::cout << "libODC_tests: optional log level as first arg. Choose from:" << std::endl;
+			std::cout << "libLua_tests: optional log level as first arg. Choose from:" << std::endl;
 			for (uint8_t i = 0; i < 7; i++)
 				std::cout << spdlog::level::level_string_views[i].data() << std::endl;
 		}
--- Code/tests/libLua_tests/Loading.cpp
+++ Code/tests/libLua_tests/Loading.cpp
@@ -1,3 +1,4 @@
+
 /*	opendatacon
  *
  *	Copyright (c) 2014:
@@ -17,37 +18,41 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-/**
+/*
+ * Loading.cpp
+ *
+ *  Created on: 9/7/2023
+ *      Author: Neil Stephens
  */
 
-#define CATCH_CONFIG_RUNNER
 
-#include "../../../opendatacon/DataConnector.cpp"
 #include <opendatacon/util.h>
+#include <opendatacon/Platform.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <json/json.h>
 #include <catch.hpp>
 
-spdlog::level::level_enum log_level = spdlog::level::off;
+extern spdlog::level::level_enum log_level;
 
-int main( int argc, char* argv[] )
+inline void TestSetup()
 {
-	int new_argc = argc;
-	char** new_argv = argv;
-	if (argc > 1)
-	{
-		std::string level_str = argv[1];
-		log_level = spdlog::level::from_str(level_str);
-		if (log_level == spdlog::level::off && level_str != "off")
-		{
-			std::cout << "libODC_tests: optional log level as first arg. Choose from:" << std::endl;
-			for (uint8_t i = 0; i < 7; i++)
-				std::cout << spdlog::level::level_string_views[i].data() << std::endl;
-		}
-		else
-		{
-			new_argc = argc - 1;
-			new_argv = argv + 1;
-		}
-	}
+	auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	auto pLibLogger = std::make_shared<spdlog::logger>("opendatacon", console_sink);
+	pLibLogger->set_level(log_level);
+	odc::spdlog_register_logger(pLibLogger);
+}
+inline void TestTearDown()
+{
+	odc::spdlog_drop_all(); // Close off everything
+}
 
-	return Catch::Session().run(new_argc, new_argv);
+TEST_CASE("LuaLoading")
+{
+	TestSetup();
+
+	//TODO: load test plugin
+	//	call into plugin
+
+
+	TestTearDown();
 }
