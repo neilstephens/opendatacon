99483c5d275f35ec23b1856a2aeeed44b5b7edde
Neil Stephens <dearknarl@gmail.com>
2025-06-24 13:06:13 +1000

    Had the unconfigured index warning backwards on DNP3MasterPort

--- Code/Ports/DNP3Port/DNP3MasterPort.cpp
+++ Code/Ports/DNP3Port/DNP3MasterPort.cpp
@@ -458,7 +458,7 @@ inline void DNP3MasterPort::LoadT(const opendnp3::ICollection<opendnp3::Indexed<
 				    || (TSO == DNP3PointConf::TimestampOverride_t::ZERO && pair.value.time.value == 0))
 					event->SetTimestamp();
 
-			bool unknown_point = pDB->Set(event);
+			bool unknown_point = !pDB->Set(event);
 			bool publish = true;
 			if(unknown_point)
 			{
