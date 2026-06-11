51a545e30246810674f4c4d8d32842d2b0c9bfa4
Neil Stephens <dearknarl@gmail.com>
2026-06-12 08:47:36 +1000

    Properly fix LuaPort/Transform cleanup
    GC in the dtor was a race with the strand
    but posting to the strand is wrong on shutdown (io_context already stopped)
    post GC in Disable() instead.

--- Code/Ports/LuaPort/LuaPort.cpp
+++ Code/Ports/LuaPort/LuaPort.cpp
@@ -41,11 +41,6 @@ LuaPort::LuaPort(const std::string& aName, const std::string& aConfFilename, con
 
 LuaPort::~LuaPort()
 {
-	pLuaSyncStrand->post([this, h{handler_tracker}]()
-		{
-			lua_gc(LuaState, LUA_GCCOLLECT);
-		});
-
 	//Wait for outstanding handlers
 	std::weak_ptr<void> tracker = handler_tracker;
 	handler_tracker.reset();
@@ -64,8 +59,12 @@ void LuaPort::Enable_()
 void LuaPort::Disable_()
 {
 	CallLuaGlobalVoidVoidFunc("Disable");
-	//Force garbage collection now. Lingering shared_ptr finalizers can block shutdown etc
-	lua_gc(LuaState,LUA_GCCOLLECT);
+	//Force garbage collection. Lingering shared_ptr finalizers can block shutdown etc
+	//post() just in case anything is queued on the strand
+	this->pLuaSyncStrand->post([this,h{handler_tracker}]()
+		{
+			lua_gc(LuaState,LUA_GCCOLLECT);
+		});
 }
 
 //Build is called while there's only one active thread, so we don't need to sync access to LuaState here
